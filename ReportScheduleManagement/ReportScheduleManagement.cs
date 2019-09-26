85805e454c2821907a12de37bdc196d2ab791a57
grizzledowski <grizzled06@rambler.ru>
2019-09-26 17:44:58 +0900

    Add project files.

---
+++ ReportScheduleManagement/ReportScheduleManagement.cs
@@ -0,0 +1,101 @@
+﻿using System.Diagnostics;
+using System.ServiceProcess;
+using System.Timers;
+using System.Runtime.InteropServices;
+
+namespace ReportScheduleManagement
+{
+    public partial class ReportScheduleManagement : ServiceBase
+    {
+        private int eventId = 1;
+
+        public enum ServiceState
+        {
+            SERVICE_STOPPED = 0x00000001,
+            SERVICE_START_PENDING = 0x00000002,
+            SERVICE_STOP_PENDING = 0x00000003,
+            SERVICE_RUNNING = 0x00000004,
+            SERVICE_CONTINUE_PENDING = 0x00000005,
+            SERVICE_PAUSE_PENDING = 0x00000006,
+            SERVICE_PAUSED = 0x00000007,
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ServiceStatus
+        {
+            public int dwServiceType;
+            public ServiceState dwCurrentState;
+            public int dwControlsAccepted;
+            public int dwWin32ExitCode;
+            public int dwServiceSpecificExitCode;
+            public int dwCheckPoint;
+            public int dwWaitHint;
+        };
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool SetServiceStatus(System.IntPtr handle, ref ServiceStatus serviceStatus);
+
+        public ReportScheduleManagement()
+        {
+            InitializeComponent();
+            eventLog1 = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists("Report Schedule Management"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource("Report Schedule Management", "Report Schedule Management Log");
+            }
+            eventLog1.Source = "Report Schedule Management";
+            eventLog1.Log = "Report Schedule Management Log";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            ServiceStatus serviceStatus = new ServiceStatus();
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_START_PENDING;
+            serviceStatus.dwWaitHint = 100000;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+
+            eventLog1.WriteEntry("Начало работы службы");
+
+            Timer timer = new Timer();
+            timer.Interval = 60000; // 60 seconds
+            timer.Elapsed += new ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_RUNNING;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+        }
+
+        public void OnTimer(object sender, ElapsedEventArgs args)
+        {
+            eventLog1.WriteEntry("Мониторинг системы", EventLogEntryType.Information, eventId++);
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("Остановка работы службы");
+
+            ServiceStatus serviceStatus = new ServiceStatus();
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_STOP_PENDING;
+            serviceStatus.dwWaitHint = 100000;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_STOPPED;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+        }
+
+        protected override void OnContinue()
+        {
+            eventLog1.WriteEntry("Продолжение работы службы");
+        }
+
+        protected override void OnPause()
+        {
+            eventLog1.WriteEntry("Служба приостановлена");
+        }
+
+        protected override void OnShutdown()
+        {
+            eventLog1.WriteEntry("Служба выключена");
+        }
+    }
+}
