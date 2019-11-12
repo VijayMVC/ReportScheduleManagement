2eacbb9003e479a24af37b7aa392a697010e351d
grizzledowski - Gavril Petrov <grizzled06@gmail.com>
2019-11-12 23:47:26 +0900

    AddWish() - доделать

--- ReportScheduleInWeb/Controllers/WishController.cs
+++ ReportScheduleInWeb/Controllers/WishController.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Xml.Serialization;
 using EncryptStringSample;
+using Newtonsoft.Json;
 using ReportScheduleInWeb.Models;
 
 namespace ReportScheduleInWeb.Controllers
@@ -58,6 +59,119 @@ namespace ReportScheduleInWeb.Controllers
                 return Json(EmptyList, JsonRequestBehavior.AllowGet);
             }
             return Json(PlaceList, JsonRequestBehavior.AllowGet);
+        }
+
+        public class parameters
+        {
+            public string name { get; set; }
+            public string value { get; set; }
+        }
+
+        public class places
+        {
+            public int id { get; set; }
+            public string startdate { get; set; }
+        }
+
+        [HttpPost]
+        public JsonResult AddWish()
+        {
+            var result = 0;
+            try
+            {
+                //Вытаскиваем данные из запроса
+                int report_type_id = Convert.ToInt32(Request.Form["report_type_id"]);
+                DateTime deadlineValue = DateTime.ParseExact(Request.Form["deadlineValue"].ToString(), "yyyy-MM-ddTHH:mm", System.Globalization.CultureInfo.InvariantCulture);
+                int attemptsCount = Request.Form["attemptsCount"].ToString() == "null" ? 0 : Convert.ToInt32(Request.Form["attemptsCount"]);
+                List<parameters> ParamList = JsonConvert.DeserializeObject<List<parameters>>(Request.Form["parameters"].ToString());
+                List<places> PlaceList = JsonConvert.DeserializeObject<List<places>>(Request.Form["places"].ToString());
+                //var places = [];
+                //var seldep = Request.Form["departments"].Split(',').Select(Int32.Parse).ToList();
+
+                ////Редактирование пользователя
+                //if (model.user_id > 0)
+                //{
+                //    if (!CheckUserLogin(model.user_login, model.user_id))
+                //    {
+                //        user Use = db.users.SingleOrDefault(x => x.user_id == model.user_id);
+                //        Use.user_login = model.user_login;
+                //        Use.user_surname = model.user_surname;
+                //        Use.user_name = model.user_name;
+                //        Use.user_patronymic = model.user_patronymic;
+                //        Use.user_isdeleted = model.user_isdeleted;
+                //        Use.user_pos_id = model.pos_id;
+                //        Use.user_role_id = model.role_id;
+
+                //        //Отделы/ТОСП пользователя ДО редактирования
+                //        List<department_users_relation> Dur = new List<department_users_relation>();
+                //        foreach (department_users_relation dur in db.department_users_relation.Where(x => x.dur_user_id == model.user_id))
+                //        {
+                //            Dur.Add(dur);
+                //        }
+
+                //        foreach (department d in db.departments)
+                //        {
+                //            if (seldep.Contains(d.dep_id))
+                //            {
+                //                if (Dur.Find(x => x.dur_department_id == d.dep_id) == null)
+                //                {
+                //                    db.department_users_relation.Add(new department_users_relation() { dur_user_id = model.user_id, dur_department_id = d.dep_id });
+                //                }
+                //            }
+                //            else
+                //            {
+                //                if (Dur.Find(x => x.dur_department_id == d.dep_id) != null)
+                //                {
+                //                    db.department_users_relation.Remove(Dur.Find(x => x.dur_department_id == d.dep_id));
+                //                }
+                //            }
+                //        }
+                //        db.SaveChanges();
+                //    }
+                //    else
+                //    {
+                //        result = 1;
+                //    }
+                //}
+                ////Создание нового пользователя
+                //else
+                //{
+                //    if (!CheckUserLogin(model.user_login, 0))
+                //    {
+                //        user Use = new user();
+                //        Use.user_login = model.user_login;
+                //        Use.user_password = encryption(model.user_password);
+                //        Use.user_surname = model.user_surname;
+                //        Use.user_name = model.user_name;
+                //        Use.user_patronymic = model.user_patronymic;
+                //        Use.user_isdeleted = model.user_isdeleted;
+                //        Use.user_pos_id = model.pos_id;
+                //        Use.user_role_id = model.role_id;
+                //        db.users.Add(Use);
+                //        db.SaveChanges();
+
+                //        foreach (department d in db.departments)
+                //        {
+                //            if (seldep.Contains(d.dep_id))
+                //            {
+                //                db.department_users_relation.Add(new department_users_relation() { dur_user_id = Use.user_id, dur_department_id = d.dep_id });
+                //            }
+                //        }
+
+                //        db.SaveChanges();
+                //    }
+                //    else
+                //    {
+                //        result = 1;
+                //    }
+                //}
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return Json(result, JsonRequestBehavior.AllowGet);
         }
     }
 }
